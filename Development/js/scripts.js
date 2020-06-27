51d8191642e56dc47e12508e1398b66f40e2daea
fahdhadhoud <66736069+fahdhadhoud@users.noreply.github.com>
2020-06-27 21:08:11 +0300

    خلصت

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,45 @@
 // السؤال الأول
 // 1
-
-// 2
-
+let  name ="fahd"; 
+let counter= 17;
 // 3 and 4
-
+console.log( console+" انا فهد عمري 17" 
+   
+);
 // 5 and 6
+counter = counter +5;
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
 
+let hobbies= ["play ", "swimm","run"];
+
+
+
+
+
+
 // 2
-
+hobbies.push("17");
+console.log(hobbies);
 // 3
-
+hobbies.pop();
+console.log(hobbies);
 // 4
+const fahd={
+    name :"wab",
+    language:"css",
+    ta:["حسين","موضي","نانسي","رهف" ]
+};
 
 // 5
 
 // 6
-
+console.log(fahd.name);
 // 7
-
+console.log(fahd.ta[0]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
